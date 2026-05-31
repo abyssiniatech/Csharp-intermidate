892326574e9ad8949fa8fb9afa41db1aaeb07625
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-31 13:19:41 +0300

    add forloop in c#:

--- tmsCore/Program.cs
+++ tmsCore/Program.cs
@@ -3,38 +3,10 @@ using System;
 class Program
 {
     static void Main(){
-    //   switch statement in c#
-    Console.WriteLine("Enter a number between 1 and 7:");
-    int day = Convert.ToInt32(Console.ReadLine());
-    switch (day)
-        {
-            case 1:
-                Console.WriteLine("Monday");
-                break;
-            case 2:
-                Console.WriteLine("Tuesday");
-                break;
-            case 3:
-                Console.WriteLine("Wednesday");
-                break;
-            case 4:
-                Console.WriteLine("Thursday");
-                break;
-            case 5:
-                Console.WriteLine("Friday");
-                break;
-            case 6:
-                Console.WriteLine("Saturday");
-                break;
-            case 7:
-                Console.WriteLine("Sunday");
-                break;
-            default:
-                Console.WriteLine("Invalid input! Please enter a number between 1 and 7.");
-                break;
-        }
-
-
+    //  forloop in c#
+     for(int i=0;i<=10;i++){
+         Console.WriteLine("The value of i is : " + i);
+     }
 
         Console.ReadLine();
 
