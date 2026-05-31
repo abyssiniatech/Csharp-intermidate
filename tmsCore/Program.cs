41d858d1e50e018c48258627cd27d18de623d711
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-31 12:30:58 +0300

    add initial commit

---
+++ tmsCore/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello, World!");
+string num="12";
+var val=Convert.ToInt32(num);
+// 'typeof' expects a compile-time type; use GetType() to get the runtime type of the variable
+Console.WriteLine(val.GetType() + ": " + val);
